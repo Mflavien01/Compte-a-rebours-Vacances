596fd582b763155f47afc92c085c707393e5a9ae
Baptiste D <63651713+Bapt5@users.noreply.github.com>
2020-10-09 14:12:25 +0200

    Update main.js
    Erreur dans les dates de 2021

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-let dates=[new Date("Oct 17, 2020 12:13:00").getTime(),new Date("Dec 19, 2020 12:13:00").getTime(),new Date("Feb 13, 2020 12:13:00").getTime(),new Date("Apr 17, 2020 12:13:00").getTime(),new Date("Jul 6, 2020 12:13:00").getTime()];
-let endDAtes=[new Date("Nov 2, 2020 00:00:00").getTime(),new Date("Jan 4, 2020 00:00:00").getTime(),new Date("Mar 1, 2020 00:00:00").getTime(),new Date("May 3, 2020 00:00:00").getTime(),new Date("Sep 1, 2020 00:00:00").getTime()];
+let dates=[new Date("Oct 17, 2020 12:13:00").getTime(),new Date("Dec 19, 2020 12:13:00").getTime(),new Date("Feb 13, 2021 12:13:00").getTime(),new Date("Apr 17, 2021 12:13:00").getTime(),new Date("Jul 6, 2021 12:13:00").getTime()];
+let endDAtes=[new Date("Nov 2, 2020 00:00:00").getTime(),new Date("Jan 4, 2021 00:00:00").getTime(),new Date("Mar 1, 2021 00:00:00").getTime(),new Date("May 3, 2021 00:00:00").getTime(),new Date("Sep 1, 2021 00:00:00").getTime()];
 var i = 0;
 var j = 0;
 
