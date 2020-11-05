a4d9048b9709a8a524696fb6f834599f9e9ad949
Baptiste D <63651713+Bapt5@users.noreply.github.com>
2020-11-05 12:29:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let dates=[new Date("Oct 17, 2020 12:13:00").getTime(),new Date("Dec 19, 2020 12:13:00").getTime(),new Date("Feb 13, 2021 12:13:00").getTime(),new Date("Apr 17, 2021 12:13:00").getTime(),new Date("Jul 6, 2021 12:13:00").getTime()];
+let dates=[new Date("Oct 17, 2020 12:13:00").getTime(),new Date("Nov 5, 2020 13:13:00").getTime(),new Date("Feb 13, 2021 12:13:00").getTime(),new Date("Apr 17, 2021 12:13:00").getTime(),new Date("Jul 6, 2021 12:13:00").getTime()];
 let endDAtes=[new Date("Nov 2, 2020 00:00:00").getTime(),new Date("Jan 4, 2021 00:00:00").getTime(),new Date("Mar 1, 2021 00:00:00").getTime(),new Date("May 3, 2021 00:00:00").getTime(),new Date("Sep 1, 2021 00:00:00").getTime()];
 var i = 0;
 var j = 0;
@@ -27,6 +27,7 @@ if (unit=="week"){
 	}
 	document.getElementById("clock").innerHTML = weeks + " semaines, " + days + " jours, " + hours + " heures, " + minutes + " minutes, " + seconds + " secondes ";
 }else if (unit=="day"){
+	var weeks = Math.floor(distance / (1000 * 60 * 60 * 24 * 7));
 	var days = Math.floor(distance /(1000 * 60 * 60 * 24));
 	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
@@ -42,6 +43,8 @@ if (unit=="week"){
 	}
 	document.getElementById("clock").innerHTML = days + " jours, " + hours + " heures, " + minutes + " minutes, " + seconds + " secondes ";
 }else if (unit=="hour"){
+	var weeks = Math.floor(distance / (1000 * 60 * 60 * 24 * 7));
+	var days = Math.floor(distance /(1000 * 60 * 60 * 24));
 	var hours = Math.floor(distance / (1000 * 60 * 60));
 	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
@@ -56,6 +59,9 @@ if (unit=="week"){
 	}
 	document.getElementById("clock").innerHTML = hours + " heures, " + minutes + " minutes, " + seconds + " secondes ";
 }else if (unit=="minute"){
+	var weeks = Math.floor(distance / (1000 * 60 * 60 * 24 * 7));
+	var days = Math.floor(distance /(1000 * 60 * 60 * 24));
+	var hours = Math.floor(distance / (1000 * 60 * 60));
 	var minutes = Math.floor(distance / (1000 * 60));
 	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 	if (weeks==0 && days==0 && hours==0 && minutes==0){
