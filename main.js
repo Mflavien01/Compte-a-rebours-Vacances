5d9c2d28014b30e593c46db1f167d78009036e77
Baptiste D <63651713+Bapt5@users.noreply.github.com>
2020-11-12 19:27:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ if (unit=="week"){
 		document.getElementById("hour").disabled=false;
 		document.getElementById("minute").disabled=false;
 	}
-	document.getElementById("clock").innerHTML = weeks + " semaines, " + days + " jours, " + hours + " heures, " + minutes + " minutes, " + seconds + " secondes ";
+	document.getElementById("clock").innerHTML = weeks + " semaines\n" + days + " jours\n" + hours + " heures\n" + minutes + " minutes\n" + seconds + " secondes ";
 }else if (unit=="day"){
 	var weeks = Math.floor(distance / (1000 * 60 * 60 * 24 * 7));
 	var days = Math.floor(distance /(1000 * 60 * 60 * 24));
