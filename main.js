1858818d9f9bef750424856d2271c900a897cda6
Baptiste D <63651713+Bapt5@users.noreply.github.com>
2020-11-13 14:36:40 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,11 +4,8 @@ var i = 0;
 var j = 0;
 
 var x = setInterval(function() {
-/*document.body.style.height="".concat(innerHeight,"px");
-document.body.style.width="auto";*/
-document.body.style.width="".concat(Math.round(innerHeight*5668/3779),"px");
-document.body.style.height="auto";
-document.body.style.backgroundSize="".concat(innerHeight,"px ",innerWidth,"px");
+document.body.style.height="".concat(innerHeight,"px");
+document.body.style.width="auto";
 
 var vacation = dates[i];
 var endVacation = endDAtes[j];
