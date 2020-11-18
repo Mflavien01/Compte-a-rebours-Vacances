146a0f60c3e1309352f7de975fd3d8c78e704091
Baptiste D <63651713+Bapt5@users.noreply.github.com>
2020-11-18 14:13:54 +0100

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
 		var response = JSON.parse(this.responseText);
-		for (let i = 0; i < 6; i++) {
+		for (let i = 0; i < response["records"].lenght; i++) {
   			if(response["records"][i]["fields"]["description"]=="Vacances de la Toussaint"){
   				dates[0]=new Date("".concat(response["records"][i]["fields"]["start_date"],"T12:13:00"));
   				endDAtes[0]=new Date("".concat(response["records"][i]["fields"]["end_date"],"T00:00:00"));
